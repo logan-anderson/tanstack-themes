9b79f6281acfd604e36f53787be0c6684a0cb808
Logan Anderson <landerson2@upei.ca>
2025-02-23 14:14:43 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import AppComponent from "./App";
+
+export const App = AppComponent;
---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig((options) => ({
+    entry: ["src/index.ts",
+        //  "src/**/*.ts", "src/**/*.tsx"
+    ],
+    format: ["cjs", "esm"],
+    dts: true,
+    clean: true,
+    sourcemap: true,
+    target: "es2022",
+    external: ["react", "react/jsx-runtime"],
+    minify: !options.watch,
+    tsconfig: "tsconfig.lib.json",
+    // banner: { js: '"use client";' },
+}));
